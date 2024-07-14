a4bd812f0184c604ea06a31f213974326d546431
Yieldone <kissbug8720@gmail.com>
2024-07-14 22:42:07 +0800

    fix: TestGetNikonLogoTmpFsPath Test code

--- resources/embed_test.go
+++ resources/embed_test.go
@@ -1,13 +1,11 @@
 package resources
 
 import (
-	"github.com/sincerefly/capybara/base/log"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestGetNikonLogoTmpFsPath(t *testing.T) {
-	path, err := CreateTemporaryLogoFile("NIKON")
+	_, err := CreateTemporaryLogoFile("NIKON")
 	assert.Nil(t, err)
-	log.Info(path)
 }
